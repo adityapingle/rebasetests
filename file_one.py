f823fed7ff66b1f1301bd8605e1a664aa17b09fa
aditya <aditya@shopsocially.com>
2016-06-14 12:33:33 +0530

    changing func definition

--- file_one.py
+++ file_one.py
@@ -4,10 +4,10 @@ experimenting on git functionalities.
 Do no execute this python script.
 '''
 
-def function_a():
+def function_a(i_need_a_variable):
     print 'this variable'
     print 'product of 5 x 5'
 
-def function_b():
+def function_b(so_do_i):
     print 'this variable'
     print 'difference between 10 and 5'
