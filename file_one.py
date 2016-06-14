f52cdb92292d69e54b2245f17af9a4481fee46db
aditya <aditya@shopsocially.com>
2016-06-14 12:32:07 +0530

    file_one introduced

---
+++ file_one.py
@@ -0,0 +1,13 @@
+'''
+This file is for testing and 
+experimenting on git functionalities.
+Do no execute this python script.
+'''
+
+def function_a():
+    print 'this variable'
+    print 'product of 5 x 5'
+
+def function_b():
+    print 'this variable'
+    print 'difference between 10 and 5'
